712d21859cf0e618752ef1f3e808aba5f9f471e7
muhammad ebeid <muhammed.ebeid@gmail.com>
2020-12-31 21:59:26 +0200

    install configure redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import statisticsReducer from './statistics/statisticsReducer';
 
-const store = createStore(statisticsReducer);
+const store = createStore(statisticsReducer, composeWithDevTools());
 
 export default store;

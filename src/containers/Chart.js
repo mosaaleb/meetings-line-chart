116364d75c94ddd5386a1e88fb699255e9f32cdb
muhammad ebeid <muhammed.ebeid@gmail.com>
2021-01-01 05:39:41 +0200

    add ability to view statistics on multi-line chart

--- src/containers/Chart.js
+++ src/containers/Chart.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Line } from 'react-chartjs-2';
 import { connect } from 'react-redux';
+import formatChartData from '../utils/formatChartData';
+
+const options = {
+  legend: {
+    display: false
+  }
+};
 
 const Chart = ({ statistics }) => (
   <div>
-    <pre>
-      {JSON.stringify(statistics, null, '\n')}
-    </pre>
+    <Line
+      options={options}
+      data={() => formatChartData(statistics)}
+    />
   </div>
 );
 
